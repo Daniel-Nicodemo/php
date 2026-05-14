0baf6018bc802495a5d0560e322a803fb16f8055
Daniel-Nicodemo <daniel.nicodemo@civiform.eu>
2026-05-14 16:06:38 +0200

    Add files via upload

--- reset.php
+++ reset.php
@@ -1,31 +1,20 @@
 <?php
-
 session_start();
 include "db.php";
-
-if(!isset($_SESSION["tavolo_id"])){
-
+if (!isset($_SESSION["tavolo_id"])){
     header("Location: index.php");
-    exit;
 }
-
 $tavolo_id = $_SESSION["tavolo_id"];
 
-//cancella i progressi di gioco per questo investigatore
+//cancella i dati per questo investigatore
 $stmt = $pdo->prepare("DELETE FROM progressi_gioco WHERE tavolo_id = ?");
-$stmt->execute($tavolo_id);
-
-//cancella le accuse dal db per questo investigatore
+$stmt->execute([$tavolo_id]);
+//cancella l'accusa dal db
 $stmt = $pdo->prepare("DELETE FROM accuse WHERE tavolo_id = ?");
-$stmt->execute($tavolo_id);
+$stmt->execute([$tavolo_id]);
 
-
-//svuota le sessioni
 $_SESSION = [];
-//distrugge il file
 session_destroy();
 
 header("Location: index.php");
 exit;
-
-?>
